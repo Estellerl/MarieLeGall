6d6e4282976caea1f5cf1f098712ce57a61ffebd
Estellerl <Estellerl@users.noreply.github.com>
2021-01-07 21:00:04 +0000

    Set up Website
    Basic React set up with basic text

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="header">
+     <h1>Marie Le Gall</h1>
+     <h2>marielegall0910@gmail.com</h2>
+     <h3>Architecte</h3>
+</div>
+    </div>
+  );
+}
+
+export default App;
